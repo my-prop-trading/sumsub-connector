78821895b0a8af1384af94aa22f94f43c8069c5f
yuriy-smplt <yuriy.p@smplt.net>
2023-10-11 16:43:29 +0300

    fixed

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -5,3 +5,4 @@ pub mod levels;
 pub mod models;
 pub mod request_signer;
 pub mod rest_client;
+pub mod validate_webhook;
--- src/rest/validate_webhook.rs
+++ src/rest/validate_webhook.rs
@@ -1,22 +1,23 @@
-use sha1::{Sha1, Digest};
-use sha2::{Sha256, Sha512};
 use hmac::{Hmac, Mac};
+use sha1::Sha1;
+use sha2::{Sha256, Sha512};
 
-
-pub fn validate_webhook (
-    x_payload_digest_alg: &String, 
-    x_signature: &String, 
-    secret_key: &String, 
-    body: &[u8]
+pub fn validate_webhook(
+    x_payload_digest_alg: &String,
+    x_signature: &String,
+    secret_key: &String,
+    body: &[u8],
 ) -> Result<(), Box<dyn std::error::Error>> {
     let algo: String = match x_payload_digest_alg.as_ref() {
-                "HMAC_SHA1_HEX" => "sha1".to_string(),
-                "HMAC_SHA256_HEX" => "sha256".to_string(),
-                "HMAC_SHA512_HEX" => "sha512".to_string(),
-                _ => return Err(Box::new(std::io::Error::new(
-                    std::io::ErrorKind::Other,
-                    "Unsupported algorithm",
-                ))),
+        "HMAC_SHA1_HEX" => "sha1".to_string(),
+        "HMAC_SHA256_HEX" => "sha256".to_string(),
+        "HMAC_SHA512_HEX" => "sha512".to_string(),
+        _ => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "Unsupported algorithm",
+            )))
+        }
     };
 
     let x_signature_bytes = hex::decode(&x_signature).unwrap();
@@ -26,28 +27,58 @@ pub fn validate_webhook (
             let mut body_signature = Hmac::<Sha1>::new_from_slice(secret_key.as_bytes())?;
             body_signature.update(body);
             body_signature.verify_slice(&x_signature_bytes[..])?;
-            
+
             return Ok(());
         }
         "sha256" => {
             let mut body_signature = Hmac::<Sha256>::new_from_slice(secret_key.as_bytes())?;
             body_signature.update(body);
             body_signature.verify_slice(&x_signature_bytes[..])?;
-            
+
             return Ok(());
         }
         "sha512" => {
             let mut body_signature = Hmac::<Sha512>::new_from_slice(secret_key.as_bytes())?;
             body_signature.update(body);
             body_signature.verify_slice(&x_signature_bytes[..])?;
-            
+
             return Ok(());
         }
 
-        _ => return Err(Box::new(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            format!("Unsupported algorithm {}", algo),
-        ))),
+        _ => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("Unsupported algorithm {}", algo),
+            )))
+        }
     };
 }
 
+#[cfg(test)]
+mod tests {
+
+    // use super::*;
+    // #[test]
+    // fn test_valid_sha256() {
+    //     let x_payload_digest_alg = "HMAC_SHA256_HEX".to_string();
+    //     let secret_key = "ns5mar5x3nf47589uaet9bk5qjs".to_string();
+    //     let body = br#"{
+    //         "applicantId" : "64fb2dde8de7445caabadb4a",
+    //         "inspectionId" : "64fb2dde8de7445caabadb4b",
+    //         "applicantType" : "individual",
+    //         "correlationId" : "56f7ebd7545bf38d8ea17f4781e053fc",
+    //         "levelName" : "proof-of-identity-level",
+    //         "sandboxMode" : true,
+    //         "externalUserId" : "level-ee704d24-dd24-45e4-b512-1a72576b1dfd",
+    //         "type" : "applicantCreated",
+    //         "reviewStatus" : "init",
+    //         "createdAt" : "2023-10-11 06:08:58+0000",
+    //         "createdAtMs" : "2023-10-11 06:08:58.546",
+    //         "clientId" : "platos_ltd"
+    //       }"#;
+    //     let x_signature =
+    //         "6fc6ad7cafb7e963ed6d4e06cdeab24a4a21239b4de83c6c2cd47b7e412eb3b4".to_string();
+
+    //     assert!(validate_webhook(&x_payload_digest_alg, &x_signature, &secret_key, body).is_ok());
+    // }
+}
