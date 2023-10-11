c06da9c18f9319447f525f174bb330c34b85aee0
yuriy-smplt <yuriy.p@smplt.net>
2023-10-11 17:20:36 +0300

    added signature test

--- src/rest/validate_webhook.rs
+++ src/rest/validate_webhook.rs
@@ -57,28 +57,15 @@ pub fn validate_webhook(
 #[cfg(test)]
 mod tests {
 
-    // use super::*;
-    // #[test]
-    // fn test_valid_sha256() {
-    //     let x_payload_digest_alg = "HMAC_SHA256_HEX".to_string();
-    //     let secret_key = "ns5mar5x3nf47589uaet9bk5qjs".to_string();
-    //     let body = br#"{
-    //         "applicantId" : "64fb2dde8de7445caabadb4a",
-    //         "inspectionId" : "64fb2dde8de7445caabadb4b",
-    //         "applicantType" : "individual",
-    //         "correlationId" : "56f7ebd7545bf38d8ea17f4781e053fc",
-    //         "levelName" : "proof-of-identity-level",
-    //         "sandboxMode" : true,
-    //         "externalUserId" : "level-ee704d24-dd24-45e4-b512-1a72576b1dfd",
-    //         "type" : "applicantCreated",
-    //         "reviewStatus" : "init",
-    //         "createdAt" : "2023-10-11 06:08:58+0000",
-    //         "createdAtMs" : "2023-10-11 06:08:58.546",
-    //         "clientId" : "platos_ltd"
-    //       }"#;
-    //     let x_signature =
-    //         "6fc6ad7cafb7e963ed6d4e06cdeab24a4a21239b4de83c6c2cd47b7e412eb3b4".to_string();
+    use super::*;
+    #[test]
+    fn test_valid_sha256() {
+        let x_payload_digest_alg = "HMAC_SHA256_HEX".to_string();
+        let secret_key = "ns5mar5x3nf47589uaet9bk5qjs".to_string();
+        let body = b"{\n  \"applicantId\" : \"64fb2dde8de7445caabadb4a\",\n  \"inspectionId\" : \"64fb2dde8de7445caabadb4b\",\n  \"applicantType\" : \"individual\",\n  \"correlationId\" : \"56f7ebd7545bf38d8ea17f4781e053fc\",\n  \"levelName\" : \"proof-of-identity-level\",\n  \"sandboxMode\" : true,\n  \"externalUserId\" : \"level-ee704d24-dd24-45e4-b512-1a72576b1dfd\",\n  \"type\" : \"applicantCreated\",\n  \"reviewStatus\" : \"init\",\n  \"createdAt\" : \"2023-10-11 06:08:58+0000\",\n  \"createdAtMs\" : \"2023-10-11 06:08:58.546\",\n  \"clientId\" : \"platos_ltd\"\n}";
+        let x_signature =
+            "6fc6ad7cafb7e963ed6d4e06cdeab24a4a21239b4de83c6c2cd47b7e412eb3b4".to_string();
 
-    //     assert!(validate_webhook(&x_payload_digest_alg, &x_signature, &secret_key, body).is_ok());
-    // }
+        assert!(validate_webhook(&x_payload_digest_alg, &x_signature, &secret_key, body).is_ok());
+    }
 }
